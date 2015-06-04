29ae0b3045f3167d13d1e00df6af5096ac326f2b
Haleigh Harper <heharper4@gmail.com>
2015-06-04 17:59:13 -0400

    deleted another migration

--- db/migrate/20150602231111_fix_match_id_names.rb
+++
@@ -1,6 +0,0 @@
-class FixMatchIdNames < ActiveRecord::Migration
-  def change
-    rename_column :recommendations, :firstMatch_id, :first_match_id
-    rename_column :recommendations, :secondMatch_id, :second_match_id
-  end
-end
