6655dac37356e9f757d3580641286d99e0ac8c57
Haleigh Harper <heharper4@gmail.com>
2015-06-15 19:16:42 -0400

    added Facebook oauth info including updated views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable,:omniauth_providers => [:facebook]
+
   has_many :friendships
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
@@ -21,8 +24,27 @@ class User < ActiveRecord::Base
 
   has_one :profile, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name...CHANGE THIS TO PROFILE
+      user.image = auth.info.image # assuming the user model has an image...CHANGE THIS TO PROFILE
+      user.create_profile!(name: auth.info.name, photo_url: auth.info.image)
+      # profile.photo_url = auth.info.image
+      # profile.name = auth.info.name
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+
 
   def recommended_as_match
     Recommendation.where('first_match_id = :user_id OR second_match_id = :user_id', user_id: self)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  #Facebook omniauth:
+  config.omniauth :facebook, "1611278772453395", "8368396ca8b39a0d07e558cb9a2990a5"
+  #config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -257,4 +260,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   # config.omniauth :facebook, "1611278772453395", "8368396ca8b39a0d07e558cb9a2990a5"
+
+
 end
